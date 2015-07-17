dd357b163aa2bb0dfb42bce365a4f3a566d5663f
Pierre Collet <pierre.collet@airbus.com>
2015-07-17 16:58:20 +0200

    Fixing configure-docker-env.sh

--- configure-docker-env.sh
+++ configure-docker-env.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 
-if [ "$1" = "probe" ]
-then
+if [ "$1" = "probe" ]; then
     echo "[+] Configuring docker environment for probe"
-    cp ./docker/probe/Dockerfile .
-    cp ./docker/probe/hosts/irma ./hosts/irma
-elif [ "$1" = 'brain' ]
-then
+    cp docker/probe/Dockerfile . && cp docker/probe/hosts/irma ./hosts/irma
+elif [ "$1" = 'brain' ]; then
     echo "[+] Configuring docker environment for brain"
-    cp ./docker/brain/Dockerfile .
-    cp ./docker/brain/hosts/irma ./hosts/irma
+    cp docker/brain/Dockerfile . && cp docker/brain/hosts/irma ./hosts/irma
 else
     echo "Usage:" $0 "[probe|brain]"
+    exit 1
+fi
+
+if [ $? -ne 0 ]; then
     exit 1
 fi
 
