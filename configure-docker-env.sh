652a57900d00b493816bf1f5e555e2011ac81ec9
Pierre Collet <pierre.collet@airbus.com>
2015-07-20 14:33:47 +0200

    adding configure script for probe and brain instead of building script

---
+++ configure-docker-env.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+if [ "$1" = "probe" ]; then
+    echo "[+] Configuring docker environment for probe"
+    cp docker/probe/Dockerfile . && cp docker/probe/hosts/irma ./hosts/irma
+elif [ "$1" = 'brain' ]; then
+    echo "[+] Configuring docker environment for brain"
+    cp docker/brain/Dockerfile . && cp docker/brain/hosts/irma ./hosts/irma
+else
+    echo "Usage:" $0 "[probe|brain]"
+    exit 1
+fi
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+echo "Done. You can now run docker build ."
+
+exit 0
